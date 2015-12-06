4efd6c525880516a8bd71229d6df7b7fef6eb406
Martijn Braam <martijn@brixit.nl>
2015-12-06 15:28:30 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='distro',
+      description='Get information about the running Linux distribution',
+      author='Martijn Braam',
+      author_email='martijn@brixit.nl',
+      url='https://github.com/MartijnBraam/distro',
+      version='0.1',
+      packages=['distro'],
+      keywords=["distro", "linux", "distribution"],
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Development Status :: 4 - Beta",
+          "Operating System :: Linux",
+          "License :: OSI Approved :: MIT License"
+      ],
+      )
